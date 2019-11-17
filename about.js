1ea1388322364b15cda38cfb98b45de3638da4eb
Rose Zhou <rmz38@cornell.edu>
2019-11-17 15:40:38 -0500

    pretty much finished?

---
+++ about.js
@@ -0,0 +1,56 @@
+var e1 = document.getElementById('cc1');
+var popup1 = document.getElementById("popup1");
+var popup2 = document.getElementById("popup2");
+var popup3 = document.getElementById("popup3");
+var popup4 = document.getElementById("popup4");
+var popup5 = document.getElementById("popup5");
+var popup6 = document.getElementById("popup6");
+e1.onmouseover = function() {
+    popup1.classList.toggle("show");
+}
+// function myFunction() {
+//     var popup = document.getElementById("popup1");
+//     popup.classList.toggle("show");
+// }
+e1.onmouseout = function() {
+    popup1.classList.toggle("show");
+}
+var e2 = document.getElementById('cc2');
+e2.onmouseover = function() {
+    popup2.classList.toggle("show");
+}
+e2.onmouseout = function() {
+    popup2.classList.toggle("show");
+}
+
+var e3 = document.getElementById('cc3');
+e3.onmouseover = function() {
+    popup3.classList.toggle("show");
+}
+e3.onmouseout = function() {
+    popup3.classList.toggle("show");
+}
+
+var e4 = document.getElementById('cc4');
+e4.onmouseover = function() {
+    popup4.classList.toggle("show");
+}
+e4.onmouseout = function() {
+    popup4.classList.toggle("show");
+}
+
+var e5 = document.getElementById('cc5');
+e5.onmouseover = function() {
+    popup5.classList.toggle("show");
+}
+e5.onmouseout = function() {
+    popup5.classList.toggle("show");
+}
+
+var e6 = document.getElementById('cc6');
+e6.onmouseover = function() {
+    popup6.classList.toggle("show");
+}
+e6.onmouseout = function() {
+    popup6.classList.toggle("show");
+}
